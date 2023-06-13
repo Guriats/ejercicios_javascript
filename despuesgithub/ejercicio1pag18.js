a5a7412a334fcbfaf6173a7024dc3489551f1c82
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-13 11:25:17 +0000

    ejercicio 1 pag 18

--- despuesgithub/ejercicio1pag18.js
+++ despuesgithub/ejercicio1pag18.js
@@ -1,3 +1,4 @@
+
 /*Usa un bucle y dos condiciones para imprimir por consola el nombre de
 los usuarios que sean menor de edad precedidos del texto
 "Usuarios menores de edad:" y otro que imprima a los usuarios mayores de edad,
@@ -5,20 +6,39 @@ precedido del texto "Usuarios mayores de edad:".*/
 
 
 const users = [
-{ name: "Tony", years: 43 },
-{ name: "Peter", years: 18 },
-{ name: "Natasha", years: 14 },
-{ name: "Bruce", years: 32 },
-{ name: "Khamala", years: 16 },
-];
+    { name: "Tony", years: 43 },
+    { name: "Peter", years: 18 },
+    { name: "Natasha", years: 14 },
+    { name: "Bruce", years: 32 },
+    { name: "Khamala", years: 16 },
+    ];
+    
+    for (i=0; i < users.length; i++)
+    
+    if (users[i].years < 18){
+        console.log ("Usuarios menores de edad");
+        console.log (users[i].name);
+    } 
+    else {
+        console.log ("Usuarios mayores de edad");
+        console.log (users[i].name);
+    }
+    
 
-for (i=0; i < users.length; i++)
 
-if (users[i].years < 18){
-    console.log ("Usuarios menores de edad");
-    console.log (users[i].name);
-} 
-else {
-    console.log ("Usuarios mayores de edad");
-    console.log (users[i].name);
+
+    //CORRECTO
+    
+    console.log("Usuarios menores de edad:");
+for (let i = 0; i < users.length; i++) {
+  if (users[i].years < 18) {
+    console.log(users[i].name);
+  }
+}
+
+console.log("Usuarios mayores de edad:");
+for (let i = 0; i < users.length; i++) {
+  if (users[i].years >= 18) {
+    console.log(users[i].name);
+  }
 }
