8fba9a346a6494207d6320203be41b7815819bfa
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-15 08:20:11 +0000

    ejercicio 8 pag 17

--- despuesgithub/ejercicio8pag17.js
+++ despuesgithub/ejercicio8pag17.js
@@ -2,10 +2,17 @@
 function sum(numberOne , numberTwo) { 
     if (numberOne > numberTwo)
     console.log ("el numberOne es mayor que number 2")
-    else { console.log ("el number 2 es mayor que el number 1");}
+    else/workspaces/ejercicios_javascript/despuesgithub { console.log ("el number 2 es mayor que el number 1");}
     if (numberOne === numberTwo){
         console.log ("estos números son iguales");
     }
 }
 
 sum (7, 9);
+
+
+
+// OTRA FORMA DE HACERLO 
+function sum(numberOne, numberTwo) {
+    return Math.max(numberOne, numberTwo);
+  }
