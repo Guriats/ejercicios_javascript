ec4de0ee90cd216216f14807d9492022599c540a
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-15 08:03:04 +0000

    numero  mas alto

--- hastagithub/ejercicio1.js
+++ hastagithub/ejercicio1.js
@@ -1,4 +1,10 @@
 //ejercicio 1.1
+
+/*1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.*/
+
 let myFavoriteHero ="Hulk";
 console.log(myFavoriteHero);
 //ejecicio 1.2
@@ -16,6 +22,9 @@ console.log (z);
 
 
 //ejercicio 3
+
+
+
 // 3.1
 let multiplicacion = 10 * 5;
 console.log (multiplicacion);
@@ -37,8 +46,13 @@ let c = 10;
 let m = 5;
 let i = c * m;
 
+
+
 // Ejercicio 6
 // 6.1
+
+
+
 const number1 = 10;
 const number2 = 20;
 const number3 = 2;
