4502e6c11aae27e10914fd82d94cf2a23f54fcba
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-17 15:38:36 +0000

    ejercicio 10 pag 17

--- ejercicio10pag17.js
+++ ejercicio10pag17.js
@@ -2,13 +2,14 @@
 //Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los
 //números de la matriz. Puedes usar este array para probar tu función:
 const numbers = [1, 2, 3, 5, 45, 37, 58];
+
 function sumAll(param) {
     let sumaTotal = 0;
     for ( let sum of param){
     sumaTotal += sum;
+    }
     console.log (sumaTotal);
     }
-}
 sumAll (numbers);
 
 
