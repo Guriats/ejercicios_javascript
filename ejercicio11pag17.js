de2ccac471d2d481f64f7ac0cf2f2ec0d2f5e409
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-17 15:31:00 +0000

    ejercicio 11 pag 17

--- ejercicio11pag17.js
+++ ejercicio11pag17.js
@@ -3,10 +3,10 @@ const numbers = [12, 21, 38, 5, 45, 37, 6];
 function average(param) {
     let total = 0;
     let entradas = 0;
-    let media = 0;
+    let media;
     for (let sum of param){
         total += sum;
-        entradas === < param.length;
+        entradas = param.length -1;
         media = total / entradas;
         console.log (media);
     }
