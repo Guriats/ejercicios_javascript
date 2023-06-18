fa350ec03c9efb183c9d9763c6ee3f25a11740a7
Guriats <135691103+Guriats@users.noreply.github.com>
2023-06-18 10:39:18 +0000

    ejercicio 12 pag 17

--- ejercicio12pag17.js
+++ ejercicio12pag17.js
@@ -2,7 +2,7 @@
 un array y cuando es un valor number lo sume y de lo
 contrario cuente la longitud del string y lo sume. 
 Puedes usar este array para probar tu función:*/
-const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+/*const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 function averageWord(param) {
     // variables para el cálculo matemático
     let total = 0;
@@ -24,23 +24,32 @@ function averageWord(param) {
             entradasNumericas = number.length;
         }
     }
-    /* const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average(param) {
-    let total = 0;
-    let entradas = 0;
-    let media;
-
-    for (let sum of param){
-        total += sum;
-    }
-        entradas = param.length;
-        media = total / entradas;
-        console.log (media);
-    }
-average (numbers); */
     console.log (sumaString);
     console.log (sum);
 }
 
-averageWord (mixedElements);
+averageWord (mixedElements);*/
+
+//
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param) {
+  let sum = 0;
+  let count = 0;
+
+  for (let element of param) {
+    if (typeof element === 'number') {
+      sum += element;
+      count++;
+    } else if (typeof element === 'string') {
+      sum += element.length;
+      count++;
+    }
+  }
+
+  return sum / count;
+}
+
+console.log(averageWord(mixedElements)); // Salida: 4.111111111111111
 
